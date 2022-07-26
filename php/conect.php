102e1bdcda889f8a8286ab280301a63635156848
Michael <102557016+Djmaykiss@users.noreply.github.com>
2022-07-26 19:32:51 -0400

    Initial commit

---
+++ php/conect.php
@@ -0,0 +1,8 @@
+$servidor      = "localhost"; 
+$usuario       = "root";
+$contrasenha   = "";
+$BD            = "miredsocial";
+
+// Creando la conexion
+
+$conexion = mysqli_connect($servidor, $usuario, $contrasenha, $BD);
